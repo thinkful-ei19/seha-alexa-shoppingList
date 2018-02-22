7dc532f9d489fa5c4880b0941b6bebebddd67c3d
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-02-22 16:33:28 -0500

    Updated shopping list

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-function()
-{}
+$(function(){
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const item = $('.js-shopping-list-entry').val();
+        $('.shopping-list').append(
+          ` <li>
+                <span class="shopping-item">${item}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`);
+
+
+
+
+      console.log(item);
+    });
+
+
+});
+
+
+
+
+
+console.log('hello');
